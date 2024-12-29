2ae2d378d46f6ff8e6866247f4444b21c3de84f6
ms_davkhar <davkharmansi29@gmail.com>
2024-12-30 00:13:22 +0530

    routing of navbar components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,32 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import FormComponent from './Components/Form/Form'; // Update the path to your Form.jsx file if needed
 
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
+import Wishlist from './Pages/Wishlist';
+import Shop from './Pages/Shop';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+
 function App() {
   return (
     <div>
-      <Navbar />
-      <FormComponent />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category="mens"/>} />
+          <Route path="/womens" element={<ShopCategory category="womens"/>} />
+          <Route path="/kids" element={<ShopCategory category="kids"/>} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/product" element={<Product />}> 
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
