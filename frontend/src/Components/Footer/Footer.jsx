1b1d5341a2987171b0102de3cd25e80b391fb705
ms_davkhar <davkharmansi29@gmail.com>
2025-01-03 23:21:18 +0530

    footer logo updated

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Footer.css'
-import footer_logo from '../Assets/logo_big.png'
+
+import footer_logo from '../Assets/my_logo.png'
 import instagram_icon from '../Assets/instagram_icon.png'
 import pinterest_icon from '../Assets/pintester_icon.png'
 import whatsapp_icon from '../Assets/whatsapp_icon.png'
@@ -10,7 +11,7 @@ const Footer = () => {
     <div className='footer'>
         <div className="footer-logo">
             <img src={footer_logo} alt="" />
-            <p>SHOPSPHERE</p>
+            
         </div>
 
         <ul className="footer-links">
